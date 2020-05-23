89b98d16b2b5b64d4f63979190e9adec3dac879c
AkashaRepo <AkashaRepo@users.noreply.github.com>
2020-05-22 18:05:55 -0700

    Output destination is now an asignable varaiable.

--- think.sh
+++ think.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 #think.sh a shell script to append a timestamped comment to an org mode file named thoughts.org
 
+OUTPUT=~/thoughts.org
+
 if # the string is empty and the file is missing
-    [ "$*" == "" ] && [ ! -f thoughts.org ];
+    [ "$*" == "" ] && [ ! -f $OUTPUT ];
 then # make a file with dummy input.
-    echo -e "* $(date)\n\tI think therefore I am.\n" >>thoughts.org
-    tail thoughts.org
+    echo -e "* $(date)\n\tI think therefore I am.\n" >>$OUTPUT
+    tail $OUTPUT
 elif # the string is empty 
     [ "$*" == "" ];
 then # simply output the contents of the file
-    tail thoughts.org
+    tail $OUTPUT
 else # write the string to the file, and display what was output to the user.
-    echo -e "* $(date)\n\t$@\n" >>thoughts.org
-    tail thoughts.org
+    echo -e "* $(date)\n\t$@\n" >>$OUTPUT
+    tail $OUTPUT
 fi
