4acbf5f1993956e81cd6375ff8c2976ff8fe80cc
AkashaRepo <AkashaRepo@users.noreply.github.com>
2020-06-23 15:00:17 -0700

    changed org mode headline depth

--- think.sh
+++ think.sh
@@ -6,13 +6,13 @@ OUTPUT=~/thoughts.org
 if # the string is empty and the file is missing
     [ "$*" == "" ] && [ ! -f $OUTPUT ];
 then # make a file with dummy input.
-    echo -e "* $(date)\n\tI think therefore I am.\n" >>$OUTPUT
+    echo -e "* Thoughts\n** $(date)\n\tI think therefore I am.\n" >>$OUTPUT
     tail $OUTPUT
 elif # the string is empty 
     [ "$*" == "" ];
 then # simply output the contents of the file
     tail $OUTPUT
 else # write the string to the file, and display what was output to the user.
-    echo -e "* $(date)\n\t$@\n" >>$OUTPUT
+    echo -e "** $(date)\n\t$@\n" >>$OUTPUT
     tail $OUTPUT
 fi
